d0cf44940a201c1f641a8e646358a084b193c6e5
Alexander Hering <dev.hering@gmail.com>
2023-05-13 12:47:57 +0200

    updated container folder name

--- export_characters.sh
+++ export_characters.sh
@@ -6,7 +6,7 @@
 # character exporter v0.1
 
 CURRENT_TS=$(date '+%Y_%b_%d-(%H-%M-%S)')
-EXPORT_PATH="/oobabooga-webui-container/text_generation_characters/EXPORTED"
+EXPORT_PATH="/text-generation-webui-container/text_generation_characters/EXPORTED"
 if [[ ! -d "${EXPORT_PATH}" ]]
 then
     mkdir "${EXPORT_PATH}"
@@ -18,7 +18,7 @@ then
 fi
 EXPORT_TS_PATH="${EXPORT_PATH}/${CURRENT_TS}"
 
-cp -R "/oobabooga-webui-container/text-generation-webui/characters" $EXPORT_TS_PATH
-cp -R "/oobabooga-webui-container/text-generation-webui/logs" $EXPORT_TS_PATH
+cp -R "/text-generation-webui-container/text-generation-webui/characters" $EXPORT_TS_PATH
+cp -R "/text-generation-webui-container/text-generation-webui/logs" $EXPORT_TS_PATH
 
 7z a -sdel "${EXPORT_PATH}/${CURRENT_TS}.7z" $EXPORT_TS_PATH
--- run_webui.sh
+++ run_webui.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-MODEL_DIR="/oobabooga-webui-container/text-generation-webui/models"
+MODEL_DIR="/text-generation-webui-container/text-generation-webui/models"
 cd "$MODEL_DIR"
 MODEL_FILE_COUNT=$(("ls -A | wc -l"))
 
 if [ $MODEL_FILE_COUNT -gt 2 ]; then
-    echo "Models found. Use '/oobabooga-webui-container/link_shared_models.sh' to relink models from shared storage."
+    echo "Models found. Use '/text-generation-webui-container/link_shared_models.sh' to relink models from shared storage."
 else
-    bash /oobabooga-webui-container/link_shared_models.sh
+    bash /text-generation-webui-container/link_shared_models.sh
 fi
 
-source /oobabooga-webui-container/venv/bin/activate
-cd /oobabooga-webui-container/text-generation-webui
+source /text-generation-webui-container/venv/bin/activate
+cd /text-generation-webui-container/text-generation-webui
 python server.py --auto-devices --chat --xformers --model-menu
