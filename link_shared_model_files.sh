f3d06d8e1a89e4104291b900c01945132585e644
Alexander Hering <dev.hering@gmail.com>
2024-01-24 22:03:11 +0100

    adjusted model linking scripts

--- link_shared_model_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in /text-generation-webui-container/machine_learning_models/MODELS/*/*.bin; do
-    echo $file
-    ln -sf $file /text-generation-webui-container/text-generation-webui/models/ 
-done
-
-find /text-generation-webui-container/text-generation-webui -xtype l -print -delete
