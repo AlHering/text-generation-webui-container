50ae46988c541b57f08779e9165598002e0601a5
Alexander Hering <dev.hering@gmail.com>
2024-04-03 00:04:47 +0200

    using new linking approach

--- run_api.sh
+++ run_api.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
 MODEL_DIR="/text-generation-webui-container/text-generation-webui/models"
 cd "$MODEL_DIR"
 MODEL_FILE_COUNT=$(("ls -A | wc -l"))
 
+source "${CONDA_DIR}/etc/profile.d/conda.sh"
+conda activate "$VENV_DIR"
+
 if [ $MODEL_FILE_COUNT -gt 2 ]; then
     echo "Models found. Use '/text-generation-webui-container/link_shared_models.sh' to relink models from shared storage."
 else
-    bash /text-generation-webui-container/link_shared_models.sh
+    python /text-generation-webui-container/link_shared_models.py
 fi
 
-source "${CONDA_DIR}/etc/profile.d/conda.sh"
-conda activate "$VENV_DIR"
 cd /text-generation-webui-container/text-generation-webui
 python server.py --auto-devices --chat --api
