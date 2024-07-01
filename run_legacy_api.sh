44c488d4ace442153c80f835bf0efe20fb10dbfb
Alexander Hering <dev.hering@gmail.com>
2024-07-01 13:13:05 +0200

    added legacy api entry point

---
+++ run_legacy_api.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MODEL_DIR="/text-generation-webui-container/text-generation-webui/models"
+cd "$MODEL_DIR"
+MODEL_FILE_COUNT=$(("ls -A | wc -l"))
+
+source "${CONDA_DIR}/etc/profile.d/conda.sh"
+conda activate "$VENV_DIR"
+
+if [ $MODEL_FILE_COUNT -gt 2 ]; then
+    echo "Models found. Use '/text-generation-webui-container/link_shared_models.sh' to relink models from shared storage."
+else
+    python /text-generation-webui-container/link_shared_models.py
+fi
+
+cd /text-generation-webui-container/text-generation-webui
+python server.py --chat --auto-devices --listen --api --extensions api gallery
+
